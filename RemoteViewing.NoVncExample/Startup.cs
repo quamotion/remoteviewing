03c506519fed973b7d89b3fefa2986680a1b9c51
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-03-31 21:53:56 +0200

    Add da demo ASP.NET Core web application which implements NoVNC.

---
+++ RemoteViewing.NoVncExample/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RemoteViewing.AspNetCore;
+using RemoteViewing.Logging;
+
+namespace RemoteViewing.NoVncExample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseVncServer(
+                "/novnc",
+                (context) => new VncContext()
+                {
+                    Password = "demo",
+                    FramebufferSource = new DummyFramebufferSource()
+                });
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
