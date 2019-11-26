f820f6c343c4ccb0d185c74c3f83206512a35317
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-11-26 19:18:30 +0100

    Fix build break

--- RemoteViewing.NoVncExample/Startup.cs
+++ RemoteViewing.NoVncExample/Startup.cs
@@ -45,8 +45,6 @@ namespace RemoteViewing.NoVncExample
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
