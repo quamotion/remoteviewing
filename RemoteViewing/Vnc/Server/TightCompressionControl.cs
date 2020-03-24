3bf25025376cc0ef2539827acc614e8270c7d96e
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-03-24 17:38:51 +0100

    Add Tight encoder

---
+++ RemoteViewing/Vnc/Server/TightCompressionControl.cs
@@ -0,0 +1,100 @@
+﻿#region License
+/*
+RemoteViewing VNC Client/Server Library for .NET
+Copyright (c) 2020 Quamotion bvba
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+
+using System;
+
+namespace RemoteViewing.Vnc.Server
+{
+    /// <summary>
+    /// Control flags used by the Tight compression protocol.
+    /// </summary>
+    [Flags]
+    internal enum TightCompressionControl : byte
+    {
+        /// <summary>
+        /// Informs the client zlib compression stream at index 0 should be reset before decoding the rectangle.
+        /// </summary>
+        ResetStream0 = 0b_0001,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 1 should be reset before decoding the rectangle.
+        /// </summary>
+        ResetStream1 = 0b_0010,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 2 should be reset before decoding the rectangle.
+        /// </summary>
+        ResetStream2 = 0b_0100,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 3 should be reset before decoding the rectangle.
+        /// </summary>
+        ResetStream3 = 0b_10100,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 0 should be used to decode the rectangle.
+        /// </summary>
+        UseStream0 = 0b_0000_0000,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 1 should be used to decode the rectangle.
+        /// </summary>
+        UseStream1 = 0b_0001_0000,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 2 should be used to decode the rectangle.
+        /// </summary>
+        UseStream2 = 0b_0010_0000,
+
+        /// <summary>
+        /// Informs the client zlib compression stream at index 3 should be used to decode the rectangle.
+        /// </summary>
+        UseStream3 = 0b_0011_0000,
+
+        /// <summary>
+        /// Informs the client the next byte specifies the <c>filter-id</c> value which tells the decoder
+        /// what filter type was used by the encoder to pre-process pixel data before the compression.
+        /// </summary>
+        ReadFilterId = 0b_0100_0000,
+
+        /// <summary>
+        /// Informs the client basic compression has been used.
+        /// </summary>
+        BasicCompression = 0b_0000_0000,
+
+        /// <summary>
+        /// Informs the client fill compression has been used.
+        /// </summary>
+        FillCompression = 0b_1000_0000,
+
+        /// <summary>
+        /// Informs the client JPEG compression has been used.
+        /// </summary>
+        JpegCompression = 0b_1001_0000,
+    }
+}
---
+++ RemoteViewing/Vnc/Server/TightEncoder.cs
@@ -0,0 +1,138 @@
+﻿#region License
+/*
+RemoteViewing VNC Client/Server Library for .NET
+Copyright (c) 2020 Quamotion bvba
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#endregion
+
+using SharpCompress.Compressors;
+using SharpCompress.Compressors.Deflate;
+using System.Diagnostics;
+using System.IO;
+
+namespace RemoteViewing.Vnc.Server
+{
+    /// <summary>
+    /// Tight encoding provides efficient compression for pixel data, using zlib compression (over 4 multiple zlib streams),
+    /// JPEG compression or fill compression, and optionally applying filters to the raw pixel data.
+    /// </summary>
+    /// <remarks>
+    /// The <see cref="TightEncoder"/> currently only implements zlib compression over a single zlib stream,
+    /// which is reset after every rectangle.
+    /// </remarks>
+    /// <seealso href="https://github.com/rfbproto/rfbproto/blob/master/rfbproto.rst#tight-encoding"/>
+    internal class TightEncoder : VncEncoder
+    {
+        /// <inheritdoc/>
+        public override VncEncoding Encoding => VncEncoding.Tight;
+
+        /// <summary>Encode a value into a byte array</summary>
+        /// <param name="buffer">The byte array to encode the 7-bit integer into</param>
+        /// <param name="startIndex">The index of the first byte for the 7-bit integer</param>
+        /// <param name="value">The value to encode into <paramref name="buffer"/></param>
+        /// <returns>Index of the first byte after the encoded value in <paramref name="buffer"/></returns>
+        public static int WriteEncodedValue(byte[] buffer, int startIndex, int value)
+        {
+            // Write out an int 7 bits at a time.  The high bit of the byte,
+            // when on, tells reader to continue reading more bytes.
+            uint v = (uint)value;
+            for (; v >= 0x80; v >>= 7, startIndex++)
+            {
+                buffer[startIndex] = (byte)(v | 0x80);
+            }
+
+            buffer[startIndex++] = (byte)v;
+
+            return startIndex;
+        }
+
+        /// <inheritdoc/>
+        public override void Send(Stream stream, VncPixelFormat pixelFormat, byte[] contents)
+        {
+            if (contents.Length < 12)
+            {
+                var compressionControl = TightCompressionControl.BasicCompression;
+                stream.WriteByte((byte)compressionControl);
+
+                // If the data size after applying the filter but before the compression is less then 12, then the data is sent as is, uncompressed.
+                byte[] encodedBuffer = new byte[4];
+                var length = WriteEncodedValue(encodedBuffer, 0, (int)contents.Length);
+                stream.Write(encodedBuffer, 0, length);
+
+                stream.Write(contents, 0, contents.Length);
+            }
+            else
+            {
+                var compressionControl = TightCompressionControl.BasicCompression
+                    | TightCompressionControl.ResetStream0
+                    | TightCompressionControl.UseStream0;
+
+                stream.WriteByte((byte)compressionControl);
+
+                using (var buffer = new MemoryStream())
+                using (var deflater = new ZlibStream(buffer, CompressionMode.Compress))
+                {
+                    // The Tight encoding makes use of a new type TPIXEL (Tight pixel). This is the same as a PIXEL for the agreed
+                    // pixel format, except where true-colour-flag is non-zero, bits-per-pixel is 32, depth is 24 and all of the bits
+                    // making up the red, green and blue intensities are exactly 8 bits wide.
+                    // In this case a TPIXEL is only 3 bytes long, where the first byte is the red component, the second byte is the
+                    // green component, and the third byte is the blue component of the pixel color value.
+                    if (pixelFormat.BitsPerPixel == 32
+                        && pixelFormat.BitDepth == 24
+                        && pixelFormat.BlueBits == 8
+                        && pixelFormat.RedBits == 8
+                        && pixelFormat.GreenBits == 8
+                        && !pixelFormat.IsPalettized)
+                    {
+                        Debug.Assert(contents.Length % 4 == 0, "The size of the raw pixel data must be a multiple of 4 when using a 32bpp pixel format.");
+
+                        for (int i = 0; i < contents.Length; i += 4)
+                        {
+                            if (i == contents.Length - 4)
+                            {
+                                deflater.FlushMode = FlushType.Finish;
+                            }
+
+                            deflater.Write(contents, i, 3);
+                        }
+                    }
+                    else
+                    {
+                        deflater.FlushMode = FlushType.Finish;
+                        deflater.Write(contents, 0, contents.Length);
+                    }
+
+                    deflater.Flush();
+
+                    byte[] encodedBuffer = new byte[4];
+                    var length = WriteEncodedValue(encodedBuffer, 0, (int)buffer.Length);
+                    stream.Write(encodedBuffer, 0, length);
+
+                    buffer.Position = 0;
+                    buffer.CopyTo(stream);
+                }
+            }
+        }
+    }
+}
